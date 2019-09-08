47ff74293ee3141be39eeff269b7b0b926b9407f
Stevertus <stevertusyt@gmail.com>
2019-09-08 16:24:24 +0200

    Form hinzugefügt + Fehler behoben

---
+++ simple_forms/Form1.vb
@@ -0,0 +1,38 @@
+﻿Imports System.Net.Sockets
+Imports System.Text
+Public Class Form1
+    Dim clientSocket As New TcpClient()
+    Dim serverStream As NetworkStream
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim text As String = TextBox1.Text
+        If text.Length = 0 Then
+            text = "Nachricht vom Client"
+        End If
+        Try
+            Dim serverStream As NetworkStream = clientSocket.GetStream()
+            Dim outStream As Byte() = Encoding.ASCII.GetBytes(text & "$")
+            serverStream.Write(outStream, 0, outStream.Length)
+            serverStream.Flush()
+
+            Dim inStream(65537) As Byte
+            serverStream.Read(inStream, 0, clientSocket.ReceiveBufferSize)
+            Dim returndata As String = Encoding.ASCII.GetString(inStream)
+            msg("Daten vom Server : " + returndata)
+
+        Catch err As Exception
+            MsgBox(err.ToString)
+        End Try
+    End Sub
+
+    Sub msg(ByVal msg As String)
+        ListBox1.Items.Add(msg)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        msg("Client Gestartet")
+        clientSocket.Close()
+        clientSocket = New TcpClient()
+        clientSocket.Connect("127.0.0.1", 8888)
+    End Sub
+End Class
--- simple_server/Module1.vb
+++ simple_server/Module1.vb
@@ -1,7 +1,44 @@
-﻿Module Module1
-
+﻿Imports System.Net.Sockets
+Imports System.Text
+Module Module1
     Sub Main()
-        Console.WriteLine("test")
+        Dim serverSocket As New TcpListener(8888)
+        Dim requestCount As Integer = 0
+        Dim clientSocket As TcpClient
+        serverSocket.Start()
+        msg("Server Gestartet")
+        clientSocket = serverSocket.AcceptTcpClient()
+
+        While (True)
+            Try
+                requestCount = requestCount + 1
+                Dim networkStream As NetworkStream = clientSocket.GetStream()
+                Dim bytesFrom(65537) As Byte
+                msg(clientSocket.ReceiveBufferSize)
+                networkStream.Read(bytesFrom, 0, clientSocket.ReceiveBufferSize)
+                Dim dataFromClient As String = System.Text.Encoding.ASCII.GetString(bytesFrom)
+                dataFromClient = dataFromClient.Substring(0, dataFromClient.IndexOf("$"))
+                msg("Daten vom Client -  " + dataFromClient)
+
+                Dim serverResponse As String = "Server response " + Convert.ToString(requestCount)
+                Dim sendBytes As Byte() = Encoding.ASCII.GetBytes(serverResponse)
+
+                networkStream.Write(sendBytes, 0, sendBytes.Length)
+                networkStream.Flush()
+                msg(serverResponse)
+            Catch ex As Exception
+                MsgBox(ex.ToString)
+            End Try
+        End While
+
+
+        clientSocket.Close()
+        serverSocket.Stop()
+        Console.ReadLine()
     End Sub
 
+    Sub msg(ByVal mesg As String)
+        mesg.Trim()
+        Console.WriteLine(" >> " + mesg)
+    End Sub
 End Module
